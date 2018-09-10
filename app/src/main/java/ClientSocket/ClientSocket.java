132252faa6e7fe5f9634f8f7d54cd06ffc8167aa
HuynhTanUIT <Nobitast2@gmail.com>
2018-09-10 22:02:43 +0700

    Draft OdynseyOne

--- app/src/main/java/ClientSocket/ClientSocket.java
+++ app/src/main/java/ClientSocket/ClientSocket.java
@@ -1,79 +1,295 @@
 package ClientSocket;
 
-import android.os.AsyncTask;
-import android.widget.TextView;
+import android.util.Log;
 
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
+
+import android.util.Log;
+
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketImpl;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.Arrays;
+
 
 /**
- * Created by tan-pc on 02/11/2016.
+ * Created by leehoa on 12/2/16.
  */
 
-public class ClientSocket extends AsyncTask<Void, Void, Void> {
+public class ClientSocket {
+    private String serverMessage;
+    /**
+     * Specify the Server Ip Address here. Whereas our Socket Server is started.
+     * */
+    public static final String SERVERIP = "192.168.4.1"; // your computer IP address
+    public static final int SERVERPORT = 1234;
+    private OnMessageReceived mMessageListener = null;
+    private boolean mRun = false;
+    Socket socket;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
+    private boolean connection = false;
 
-    String dstAddress;
-    int dstPort;
-    String response = "";
-    TextView textResponse;
+    String IP = "192.168.0.138"; // your computer IP address
+    int PORT = 9999;
 
-   public ClientSocket(String addr, int port, TextView textResponse) {
-        dstAddress = addr;
-        dstPort = port;
-        this.textResponse = textResponse;
+    /**
+     *  Constructor of the class. OnMessagedReceived listens for the messages received from server
+     */
+    public ClientSocket(final OnMessageReceived listener,String IP, int PORT)
+    {
+        mMessageListener = listener;
+        this.IP = IP;
+        this.PORT = PORT;
     }
 
-    @Override
-    protected Void doInBackground(Void... arg0) {
+    /**
+     * Sends the message entered by client to the server
+     * @param message text entered by client
+     */
+    public void sendMessage(String message){
+        if (out != null && !out.checkError()) {
+            System.out.println("message: "+ message);
+            out.println(message);
+            out.flush();
+        }
+    }
 
-        Socket socket = null;
+    public void sendMessage(byte[] message){
+        if (out != null && !out.checkError()) {
+            System.out.println(message);
+            out.println(message);
+            out.flush();
+        }
+    }
+    public  boolean sendBytes(byte[] myByteArray,int i) throws IOException {
+        try {
+            sendBytes(myByteArray, 0, myByteArray.length);
+        }catch (Exception e){
+            Log.e("Tan log sendBYtes", e.toString());
+            return false;
+        }
+        return true;
+    }
+    public  void sendBytes(byte[] myByteArray) throws IOException {
+        try {
+            sendBytes(myByteArray, 0, myByteArray.length);
+        }catch (Exception e){
+            Log.e("Tan log sendBYtes", e.toString());
+        }
+
+    }
+//    public boolean sendBytes(ByteBuffer myByteArray) throws IOException {
+//
+//
+//    }
+    // private DeviceInfo.SERIAL_CMD TxCmdCode;
+    //private UInt16 TxSeq;
+
+
+    public boolean WriteData(ByteBuffer _sendBuf){
+        try {
+            DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            dout.write(_sendBuf.array());
+            dout.close();
+        }catch (Exception e)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void sendBytes(byte[] myByteArray, int start, int len) throws IOException {
+        if (len < 0) {
+            throw new IllegalArgumentException("Negative length not allowed");
+        }
+        if (start < 0 || start >= myByteArray.length)
+            throw new IndexOutOfBoundsException("Out of bounds: " + start);
+        // Other checks if needed.
+
+        // May be better to save the streams in the support class;
+        // just like the socket variable.
+        OutputStream out = socket.getOutputStream();
+        DataOutputStream dos =new DataOutputStream(out);
+        // dos.writeInt(len);
+        if (len > 0) {
+            //  buffer.flip();
+            dos.write(myByteArray,start,len);
+            //  buffer.clear();
+            //    Log.e("SentBytes", "Array: " + Arrays.toString(myByteArray) + "Lengh: " + myByteArray.length );
+
+            //Log.e("Sent", "Array: " + myByteArray.clone() + " start:  " + start +" lenght: " + len );
+            //  Log.e("sent1", "Lenght: " + myByteArray.length + "  " );
+        }
+        else
+        {
+
+        }
+        //   Log.e("sendBytes", "Da Send " + myByteArray);
+
+    }
+
+    public void stopsocket() throws IOException {
+        if(connection == true )
+        {
+            socket.close();
+        }else
+        {
+            Log.e("TCP SI Error", "SI: Error Tan");
+        }
+    }
+    public boolean isConnect(){
+        // Log.e("TCP SI Client", "SI: Connecting...");
+//        if (connection ==true)
+//        {
+//            boolean input = socket.isInputShutdown();
+//            boolean output = socket.isOutputShutdown();
+//            boolean connect = socket.isConnected();
+//            boolean bound = socket.isBound();
+//        }
+        //  boolean input = socket.isInputShutdown();
+        ////  boolean output = socket.isOutputShutdown();
+        //  boolean connect = socket.isConnected();
+        //  boolean bound = socket.isBound();
+        //   Log.e("RESPONSE FROM SERVER", "Is Input Shutdown: " + input + " Is Outputshutdown: " + output+" Is Connect: "+connect+" Is bound: " +bound );
+        return connection == true;
+    }
+
+    public void stopClient() throws IOException {
+//        mRun = false;
+//        if(out !=null){
+//            out.flush();
+//            out.close();
+//        }
+//        if(in!=null)
+//        {
+//            in.close();
+//        }
 
         try {
-            socket = new Socket(dstAddress, dstPort);
-
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
-                    1024);
-            byte[] buffer = new byte[1024];
-
-            int bytesRead;
-            InputStream inputStream = socket.getInputStream();
-
-         /*
-          * notice: inputStream.read() will block if no data return
-          */
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                byteArrayOutputStream.write(buffer, 0, bytesRead);
-                response += byteArrayOutputStream.toString("UTF-8");
-            }
-
-        } catch (UnknownHostException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            response = "UnknownHostException: " + e.toString();
+            socket.shutdownOutput();
+            socket.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-            response = "IOException: " + e.toString();
-        } finally {
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
         }
-        return null;
+
+//        socket=null;
+//        in=null;
+//        out=null;
+    }
+    public void run() {
+        mRun = true;
+
+        try {
+            //here you must put your computer's IP address.
+            InetAddress serverAddr = InetAddress.getByName(IP);
+            Log.e("TCP SI Client", "SI: Connecting...");
+            connection = false;
+            //create a socket to make the connection with the server
+            socket = new Socket(serverAddr, PORT);
+
+            try {
+
+                //send the message to the server
+                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+                Log.e("TCP SI Client", "SI: Sent.");
+                Log.e("TCP SI Client", "SI: Done.");
+                connection = true;
+//                serverMessage = in.readLine();
+
+                //receive the message which the server sends back
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                //in this while the client listens for the messages sent by the server
+//                while (mRun) {
+//                   //
+//
+//                    if (serverMessage != null && mMessageListener != null) {
+//                        //call the method messageReceived from MyActivity class
+//                        //smMessageListener.messageReceived(serverMessage);
+//                        //Log.e("RESPONSE FROM SERVER", "S: Received Message: '" + serverMessage + "'");
+//                    }
+//                    serverMessage = null;
+//                }
+//                connection = true;
+            }
+            catch (Exception e)
+            {
+
+                connection = false;
+                Log.e("TCP SI Error", "SI: Error", e);
+                e.printStackTrace();
+            }
+            finally
+            {
+                connection = true;
+
+                //the socket must be closed. It is not possible to reconnect to this socket
+                // after it is closed, which means a new socket instance has to be created.
+                //socket.close();
+            }
+
+        } catch (Exception e) {
+            connection = false;
+            Log.e("TCP SI Error", "SI: Error", e);
+        } finally {
+//            if (socket != null){
+//                try {
+//                    socket.close();
+//                } catch (IOException e) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            if (out != null){
+//                out.close();
+//            }
+//
+//            if (in != null){
+//                try {
+//                    in.close();
+//                } catch (IOException e) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+//            }
+        }
     }
 
-    @Override
-    protected void onPostExecute(Void result) {
-        textResponse.setText(response);
-        super.onPostExecute(result);
+    public boolean connectServer() {
+        return connection;
     }
 
+    public interface OnMessageReceived {
+        void messageReceived(String message);
+
+    }
 }
--- app/src/main/java/SettingsSQLite/SqliteHelper.java
+++ app/src/main/java/SettingsSQLite/SqliteHelper.java
@@ -9,15 +9,18 @@ import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteStatement;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.Switch;
 import android.widget.Toast;
 
-import com.example.tan_pc.navigationdraweractivity.MainActivity;
+import com.example.tan_pc.navigationdraweractivity.Activity.MainActivity;
 
 import java.net.PortUnreachableException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by TAN-PC on 10/12/2016.
@@ -37,17 +40,25 @@ public class SqliteHelper extends SQLiteOpenHelper {
     public static final String KEY_LANGUAGES ="Languages";
     public static final String KEY_THEMES ="Themes";
 
-    public static final String TABLE_BINARY_192 ="Binary192Valves";
-    public static final String ID_BIN192="idBin";
-    public static final String NAME_BIN192="NameBin";
-    public static final String WIDTH_BIN192_="WidthBin";
-    public static final String HEIGHT_BIN192_="HeightBin";
-    public static final String IMAGE_BIN192="ImageBin";
+    public static final String TABLE_IMAGE ="images";
+    public static final String KEY_PATH ="path";
+    public static final String KEY_ID_IMANGE ="_id_image";
 
-    public static final String TABLE_HOME = "Home";
-    public static final String ID_HOME="IdHome";
-    public static final String TIMES_HOME="TimesHome";
-    public static final String AFTER_HOME="AfterHome";
+    public static final String TABLE_IMAGES_DIRECTORY ="images_directory";
+    public static final String KEY_DIRECTORY ="path_directory";
+    public static final String KEY_ID_DIRECTORY="_id_directory";
+
+//    public static final String TABLE_BINARY_192 ="Binary192Valves";
+//    public static final String ID_BIN192="idBin";
+//    public static final String NAME_BIN192="NameBin";
+//    public static final String WIDTH_BIN192_="WidthBin";
+//    public static final String HEIGHT_BIN192_="HeightBin";
+//    public static final String IMAGE_BIN192="ImageBin";
+//
+//    public static final String TABLE_HOME = "Home";
+//    public static final String ID_HOME="IdHome";
+//    public static final String TIMES_HOME="TimesHome";
+//    public static final String AFTER_HOME="AfterHome";
 
 
 
@@ -55,6 +66,30 @@ public class SqliteHelper extends SQLiteOpenHelper {
 //        super(context, PROJECTDATABASE, null, 1);
 //        Log.e("DATABASE OPERATION","DATABASE CREATE /OPENED...");
 //    }
+    public void deleteImage(String path) {
+        Log.i(TAG, "MyDatabaseHelper.updateNote ... ");
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_IMAGE, KEY_PATH + " = ?",
+                new String[]{path});
+        db.close();
+    }
+
+    public long CountImage() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        long cnt  = DatabaseUtils.queryNumEntries(db, TABLE_IMAGE);
+        Log.i("Data1",""+cnt);
+        db.close();
+        return cnt;
+    }
+
+    public void addImage(String path) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_PATH, path);
+        db.insert(TABLE_IMAGE, null, values);
+        db.close();
+    }
 
     public SqliteHelper (Context context,String name, SQLiteDatabase.CursorFactory factory,int version){
         super(context,name,factory,version);
@@ -73,43 +108,45 @@ public class SqliteHelper extends SQLiteOpenHelper {
     }
 
     //count Row
-    public long CountImage192Valves() {
-        SQLiteDatabase db = this.getReadableDatabase();
-        long cnt  = DatabaseUtils.queryNumEntries(db, TABLE_BINARY_192);
-        db.close();
-        return cnt;
-    }
-    public void CreateHome(SQLiteDatabase db){
-        String CREATE_TABLE_HOME="CREATE TABLE IF NOT EXISTS "+TABLE_HOME+" ("
-                +ID_HOME+" INTEGER PRIMARY KEY AUTOINCREMENT, "
-                +TIMES_HOME+" INT,"
-                +AFTER_HOME+" INT)";
-        db.execSQL(CREATE_TABLE_HOME);
-        InitialHome(db);
-    }
+//    public long CountImage192Valves() {
+//        SQLiteDatabase db = this.getReadableDatabase();
+//        long cnt  = DatabaseUtils.queryNumEntries(db, TABLE_BINARY_192);
+//        db.close();
+//        return cnt;
+//    }
+//    public void CreateHome(SQLiteDatabase db){
+//        String CREATE_TABLE_HOME="CREATE TABLE IF NOT EXISTS "+TABLE_HOME+" ("
+//                +ID_HOME+" INTEGER PRIMARY KEY AUTOINCREMENT, "
+//                +TIMES_HOME+" INT,"
+//                +AFTER_HOME+" INT)";
+//        db.execSQL(CREATE_TABLE_HOME);
+//        InitialHome(db);
+//    }
     @Override
     public void onCreate(SQLiteDatabase db) {
         //Nên gọi các phương thức tạo bảng
-        CreateHome(db);
+//        CreateHome(db);
         CreateSettingsAndInitialValues(db);
         CreateBinary_192Valves(db);
+        CreateImageTable(db);
+        CreateImageDirectory(db);
     }
     public void InitialHome(SQLiteDatabase db){
 
         ContentValues contentValues = new ContentValues();
-        contentValues.put(ID_HOME, 0);
-        contentValues.put(TIMES_HOME, 5);
-        contentValues.put(AFTER_HOME, 5);
-        db.insert(TABLE_HOME, null, contentValues);
+//        contentValues.put(ID_HOME, 0);
+//        contentValues.put(TIMES_HOME, 5);
+//        contentValues.put(AFTER_HOME, 5);
+//        db.insert(TABLE_HOME, null, contentValues);
 
     }
     public boolean updateHome( int times,int after) {
         SQLiteDatabase db= this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(TIMES_HOME,times );
-        contentValues.put(AFTER_HOME, after);
+//        contentValues.put(TIMES_HOME,times );
+//        contentValues.put(AFTER_HOME, after);
         try {
-            db.update(TABLE_HOME, contentValues, "_id = " + 0, null);
+//            db.update(TABLE_HOME, contentValues, "_id = " + 0, null);
         } catch (Exception ex) {
             return false;
         }
@@ -118,35 +155,34 @@ public class SqliteHelper extends SQLiteOpenHelper {
     //Delete Binary Image by ID
     public void DETELE__BINARY_192Valves(int id){
         SQLiteDatabase db=getWritableDatabase();
-        db.execSQL("DELETE FROM "+TABLE_BINARY_192+" WHERE "+ID_BIN192+"=" +String.valueOf(id));
+//        db.execSQL("DELETE FROM "+TABLE_BINARY_192+" WHERE "+ID_BIN192+"=" +String.valueOf(id));
     }
 
     public void CreateValvesTable(SQLiteDatabase db){
 
     }
+
+    public  void CreateImageTable(SQLiteDatabase db) {
+        String CREATE_TABLE_IMAGE="CREATE TABLE IF NOT EXISTS "+TABLE_IMAGE+"("
+                +KEY_ID_IMANGE+" INTEGER PRIMARY KEY,"
+                +KEY_PATH+" TEXT);";
+        db.execSQL(CREATE_TABLE_IMAGE);
+    }
+
     //insert Binary Image To Database
     public void INSERT_BINARY_192Valves(String name,int width,int height, byte[] image){
             SQLiteDatabase db=getWritableDatabase();
-            String sql="INSERT INTO "+TABLE_BINARY_192+" VALUES(NULL,?,?,?,?)";
-            SQLiteStatement statement = db.compileStatement(sql);
-            statement.clearBindings();
-            statement.bindString(1,name);
-            statement.bindLong(2,width);
-            statement.bindLong(3,height);
-            statement.bindBlob(4,image);
-            statement.executeInsert();
+//            String sql="INSERT INTO "+TABLE_BINARY_192+" VALUES(NULL,?,?,?,?)";
+//            SQLiteStatement statement = db.compileStatement(sql);
+//            statement.clearBindings();
+//            statement.bindString(1,name);
+//            statement.bindLong(2,width);
+//            statement.bindLong(3,height);
+//            statement.bindBlob(4,image);
+//            statement.executeInsert();
     }
 
-
     public void CreateBinary_192Valves(SQLiteDatabase db){
-
-            String CREATE_TABLE_BINARY192="CREATE TABLE IF NOT EXISTS "+TABLE_BINARY_192+" ("
-                    +ID_BIN192+" INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    +NAME_BIN192+" TEXT,"
-                    +WIDTH_BIN192_+" INT,"
-                    +HEIGHT_BIN192_+" INT,"
-                    +IMAGE_BIN192+" BLOB UNIQUE)";
-            db.execSQL(CREATE_TABLE_BINARY192);
 
     }
     public void CreateSettingsAndInitialValues(SQLiteDatabase db)
@@ -165,8 +201,16 @@ public class SqliteHelper extends SQLiteOpenHelper {
             db.execSQL(CREATE_TABLE_SETTINGS);
             insertSettings(db);
             Log.e("DATABASE OPERATION","TABLE SETTINGS CREATED");
-
     }
+    public void CreateImageDirectory(SQLiteDatabase db){
+        String CREATE_TABLE_IMAGE_DIRECTORY="CREATE TABLE IF NOT EXISTS "+TABLE_IMAGES_DIRECTORY+"("
+                +KEY_ID_DIRECTORY+" INTEGER PRIMARY KEY, "
+                +KEY_DIRECTORY+" TEXT);";
+        db.execSQL(CREATE_TABLE_IMAGE_DIRECTORY);
+        insertDirectory(db);
+        Log.e("DATABASE OPERATION","TABLE DIRECTORY CREATED");
+    }
+
 //    public void insertSettingsValues (SQLiteDatabase db){
 //        ContentValues contentValues =new ContentValues();
 //        contentValues.put(KEY_ID, 0);
@@ -187,8 +231,11 @@ public class SqliteHelper extends SQLiteOpenHelper {
         //Thực hiện các thao tác chỉnh sửa cấu trúc bảng của DB
 
             db.execSQL("DROP TABLE IF EXISTS "+TABLE_SETTINGS);
-            db.execSQL("DROP TABLE IF EXISTS "+TABLE_BINARY_192);
-            onCreate(db);
+//            db.execSQL("DROP TABLE IF EXISTS "+TABLE_BINARY_192);
+            db.execSQL("DROP TABLE IF EXISTS "+TABLE_IMAGE);
+
+            db.execSQL("DROP TABLE IF EXISTS "+TABLE_IMAGES_DIRECTORY);
+        onCreate(db);
 
     }
 //    //thêm bản ghi
@@ -196,15 +243,26 @@ public class SqliteHelper extends SQLiteOpenHelper {
 
             ContentValues contentValues = new ContentValues();
             contentValues.put(KEY_ID, 0);
-            contentValues.put(KEY_VALVES, 0);
-            contentValues.put(KEY_ROWS, 150);
-            contentValues.put(KEY_IMAGES, 2000);
+            contentValues.put(KEY_VALVES, 320);
+            contentValues.put(KEY_ROWS, 1);
+            contentValues.put(KEY_IMAGES, 1);
             contentValues.put(KEY_THREHOLD, 128);
-            contentValues.put(KEY_IP, "192.168.1.1");
-            contentValues.put(KEY_PORT, 8888);
+            contentValues.put(KEY_IP, "192.168.4.1");
+            contentValues.put(KEY_PORT, 1234);
             contentValues.put(KEY_LANGUAGES, 0);
             contentValues.put(KEY_THEMES, 0);
             db.insert(TABLE_SETTINGS, null, contentValues);
+
+    }
+    public void insertDirectory(SQLiteDatabase db){
+
+        ContentValues contentValues = new ContentValues();
+        String Imagefoler_dir= Environment.getExternalStorageDirectory()
+                .getPath() + "/folder_name";
+        contentValues.put(KEY_ID_DIRECTORY, 0);
+        contentValues.put(KEY_DIRECTORY, Imagefoler_dir);
+
+        db.insert(TABLE_IMAGES_DIRECTORY, null, contentValues);
 
     }
     //Cập nhật bản ghi
@@ -222,6 +280,27 @@ public class SqliteHelper extends SQLiteOpenHelper {
         try {
             db.update(TABLE_SETTINGS, contentValues, "_id = " + 0, null);
         } catch (Exception ex) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean updateDirectory( String directory_path) {
+        SQLiteDatabase db= this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(KEY_DIRECTORY, directory_path);
+//        contentValues.put(KEY_ROWS, rows);
+//        contentValues.put(KEY_IMAGES, images);
+//        contentValues.put(KEY_THREHOLD, threhold);
+//        contentValues.put(KEY_IP, ip);
+//        contentValues.put(KEY_PORT, port);
+//        contentValues.put(KEY_LANGUAGES, languages);
+//        contentValues.put(KEY_THEMES, themes);
+        try {
+            db.update(TABLE_IMAGES_DIRECTORY, contentValues, "_id_directory = " + 0, null);
+            Log.e(TAG, "updateDirectory: "+ directory_path.toString());
+        } catch (Exception ex) {
+            Log.e(TAG, "updateDirectory: "+ ex.toString());
             return false;
         }
         return true;
